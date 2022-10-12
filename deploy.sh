e7ecd156f7b05b9ca6a3e2ab71c93cbd86747a19
Yaroslav Kulinich <55299766+burevestnik-png@users.noreply.github.com>
2022-10-12 21:26:16 +0300

    Added deployment for frontend

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,8 @@
+cd frontend-for-dummies
+npm run build:dev
+cd ..
+cp -R frontend-for-dummies/build/* collection/src/main/webapp/
+
 ./gradlew build
 cp collection/build/libs/*.war deployments/
 cp demography/build/libs/*.war deployments/
