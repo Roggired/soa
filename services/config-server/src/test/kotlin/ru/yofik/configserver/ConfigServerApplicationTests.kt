4404a5da28460b171dacee18fbc555ad1499b5f5
Yaroslav Kulinich <yaroslavsoldat2@gmail.com>
2022-11-25 20:40:22 +0300

    ok

---
+++ services/config-server/src/test/kotlin/ru/yofik/configserver/ConfigServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.yofik.configserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
